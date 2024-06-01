714577abba7c895dda6c48080a740000839a079d
Njugi92 <kenkaruma.kk@gmail.com>
2024-06-02 02:51:03 +0300

    My photo gallery RESTAPI

---
+++ src/data.ts
@@ -0,0 +1,44 @@
+export const slides = [
+    {
+        src: 'https://images.unsplash.com/photo-1715645943531-a57960d41818?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'slide title 1',
+        description: 'sLIDE DESCRIPTION ONE'
+    } ,
+    {
+        src: 'https://images.unsplash.com/photo-1715942163364-5aa9e6d66bb4?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'Slides title 2',
+        description: 'sLIDE DESCRIPTION TWO'
+    } ,
+    {
+        src: 'https://plus.unsplash.com/premium_photo-1704686580778-680853cd8d29?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8',
+        title: 'Slide title 3',
+        description: 'sLIDE DESCRIPTION THREE',
+    } ,
+    {
+        src: 'https://images.unsplash.com/photo-1715521801502-42db7b79573b?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'Slide title 4',
+        description: 'sLIDE DESCRIPTION FOUR',
+    } ,
+    {
+        src: 'https://images.unsplash.com/photo-1715521801502-42db7b79573b?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'Slide title 5',
+        description: 'sLIDE DESCRIPTION FIVE',
+    } ,
+    {
+        src: 'https://images.unsplash.com/photo-1715521801502-42db7b79573b?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'Slide title 6',
+        description: 'sLIDE DESCRIPTION SIX',
+    } ,
+    {
+        src: 'https://images.unsplash.com/photo-1715833027976-7183e02993da?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'Slide title 7',
+        description: 'sLIDE DESCRIPTION SEVEN',
+    } ,
+    {
+        src: 'https://images.unsplash.com/photo-1715833027976-7183e02993da?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'Slide title 8',
+        description: 'sLIDE DESCRIPTION EIGHT',
+    } 
+
+
+];
